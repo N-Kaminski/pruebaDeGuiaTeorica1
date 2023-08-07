aef962b125009f1830050b2a92810a76fc873790
Nico <murek38@gmail.com>
2023-08-07 20:23:29 -0300

    Initial commit

---
+++ src/pruebadeguiateorica1/PruebaDeGuiaTeorica1.java
@@ -0,0 +1,16 @@
+
+package pruebadeguiateorica1;
+
+// @author Nicolas Kaminski 
+
+
+public class PruebaDeGuiaTeorica1 {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Veremos que pasa...");
+
+    }       // lave main
+
+}       // llave clase
---
+++ src/pruebadeguiateorica1/test/Codeando1.java
@@ -0,0 +1,10 @@
+
+package pruebadeguiateorica1.test;
+
+/** @author Nicolas Kaminski */
+
+public class Codeando1 {
+
+
+
+}  // LLAVE DE CLASE
